e30ce3e5f1448b9be1075fb929cca4e48362bd6a
Roman-Kachura <Roman-Kachura@users.noreply.github.com>
2021-12-09 16:18:55 +0100

    Update 09.12.2021

--- src/components/c2-main/m3-workspage/WorksItem.tsx
+++ src/components/c2-main/m3-workspage/WorksItem.tsx
@@ -1,5 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import style from './Works.module.scss';
+import {faEye} from "@fortawesome/free-solid-svg-icons";
 
 type worksItemPropsType = {
     text:string
@@ -11,7 +13,9 @@ export const WorksItem = ({text,title}:worksItemPropsType) => {
         <div className={style.worksItem}>
                 <div className={style.image}>
                     <div className={style.imageWrapper}/>
-                    <a href='' className={style.link}>C</a>
+                    <a href='' className={style.link}>
+                        <i><FontAwesomeIcon icon={faEye}/></i>
+                    </a>
                 </div>
 
                 <div className={style.description}>
